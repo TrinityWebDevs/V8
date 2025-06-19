b0e09e24d291dbc3db01c0676ee18a052dfa9428
khantnandeesh <148197396+khantnandeesh@users.noreply.github.com>
2025-06-19 11:19:21 +0530

    Update index.js->proxy enabled

--- Backend/index.js
+++ Backend/index.js
@@ -34,7 +34,7 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
 const projectOnlineUsers = {};
 const projectAIChatHistories = {};
-
+app.set('trust proxy', 1);
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
